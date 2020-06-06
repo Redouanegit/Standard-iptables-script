18a67a054187de5dbd2a14f412cffc1748f0e13d
red <django777@gmail.com>
2020-06-06 14:00:27 +0200

    iptables standard

---
+++ iptables.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+# Clear all rules
+sudo iptables -t filter -F
+sudo iptables -t filter -X
+
+# Deny everything by default
+sudo iptables -t filter -P INPUT DROP
+sudo iptables -t filter -P FORWARD DROP
+sudo iptables -t filter -P OUTPUT DROP
+
+# Authorize already established connexions
+sudo iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -A OUTPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -t filter -A INPUT -i lo -j ACCEPT
+sudo iptables -t filter -A OUTPUT -o lo -j ACCEPT
+
+# Authorize ICMP (Ping)
+sudo iptables -t filter -A INPUT -p icmp -j ACCEPT
+sudo iptables -t filter -A OUTPUT -p icmp -j ACCEPT
+
+# Authorize SSH
+sudo iptables -t filter -A INPUT -p tcp --dport 22 -j ACCEPT
+sudo iptables -t filter -A OUTPUT -p tcp --dport 22 -j ACCEPT
+
+# Authorize DNS
+sudo iptables -t filter -A OUTPUT -p tcp --dport 53 -j ACCEPT
+sudo iptables -t filter -A OUTPUT -p udp --dport 53 -j ACCEPT
+sudo iptables -t filter -A INPUT -p tcp --dport 53 -j ACCEPT
+sudo iptables -t filter -A INPUT -p udp --dport 53 -j ACCEPT
+
+# Authorize HTTP
+sudo iptables -t filter -A OUTPUT -p tcp --dport 80 -j ACCEPT
+sudo iptables -t filter -A INPUT -p tcp --dport 80 -j ACCEPT
+
+# Authorize HTTPS
+sudo iptables -t filter -A OUTPUT -p tcp --dport 443 -j ACCEPT
+sudo iptables -t filter -A INPUT -p tcp --dport 443 -j ACCEPT
+
+# Authorize FTP
+sudo iptables -t filter -A OUTPUT -p tcp --dport 20:21 -j ACCEPT
+sudo iptables -t filter -A INPUT -p tcp --dport 20:21 -j ACCEPT
+
+
+
